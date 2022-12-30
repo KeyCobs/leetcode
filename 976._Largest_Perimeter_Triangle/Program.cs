a228b415e22f433eb1eef93d30176da0e4c7096a
Kevin Jacobs <kevinjacobs@live.nl>
2022-12-30 10:26:40 +0100

    Added Larges Permiter

---
+++ 976._Largest_Perimeter_Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] case1 = { 2, 2, 1 };
+int[] case2 = { 1, 2, 1,10 };
+int[] case3 = { 3, 6, 2, 3 };
+
+Console.WriteLine(LargestPerimeter(case1) + " Correct answer is: 5");
+Console.WriteLine(LargestPerimeter(case2) + " Correct answer is: 0");
+Console.WriteLine(LargestPerimeter(case3) + " Correct answer is: 8");
+
+
+int LargestPerimeter(int[] nums)
+{
+    //1: Check if it's a triangle
+    if (nums.Length < 3)
+    {
+        return 0;
+    }
+    //Put it in Descending order. Takes the biggest permiter first
+    Array.Sort(nums);
+    Array.Reverse(nums);
+
+    for (int i = 0; i < nums.Length - 2; i++)
+    {
+
+        if (nums[i] < (nums[i + 1] + nums[i + 2]))
+        {
+            return nums[i] + nums[i + 1] + nums[i + 2];
+        }
+    }
+    //If nothing makes a triangle then just return 0
+    return 0;
+}
+
